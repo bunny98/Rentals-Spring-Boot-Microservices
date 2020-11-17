fac955f7fcf5d9feacd7ef321f0b125d1aba4e7b
bunny98 <kavyansh.chourasia98@gmail.com>
2020-11-17 16:55:55 +0530

    ADDED DISCOVERY SERVER

---
+++ Discovery-Server/src/main/java/com/rentals/DiscoveryServer/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.rentals.DiscoveryServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
--- orders-service/src/main/java/com/rentals/ordersservice/controller/OrderController.java
+++ orders-service/src/main/java/com/rentals/ordersservice/controller/OrderController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/order")
